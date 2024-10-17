9203b952212e83a54e8e860805e8ab90e6623b68
Peter Buckingham <peterbuckingham177@gmail.com>
2024-10-17 22:24:45 +0200

    Built the todo funtionality with React Hooks

--- todo_app/src/App.js
+++ todo_app/src/App.js
@@ -1,11 +1,26 @@
+import React, {useState} from 'react';
 import './App.css';
 
 function App() {
+  const [todos, setTodos] = useState(['Take Dogs for a walk', 'Take the Rubbish out', 'Peter test']);
+
+
   return (
     <div className="App">
       <h1>Hello World</h1>
       <input />
       <button>Add Todo</button>
+
+      <ul>
+        {todos.map(todo => (
+          <li>{todo}</li>
+        ))}
+        
+        <li></li>
+        <li></li>
+      </ul>
+
+
     </div>
   );
 }
