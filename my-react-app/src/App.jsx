f23fd8f59177a0f1958b24852252071d017cac81
Peter Buckingham <peterbuckingham177@gmail.com>
2024-10-29 19:56:33 +0200

    my fist commit

---
+++ my-react-app/src/App.jsx
@@ -0,0 +1,12 @@
+import Header from "./assets/Header";
+import Footer from "./assets/Footer";
+import Food from "./Food";
+
+export default function App() {
+  return (
+  <>
+    <Header/>
+    <Food/>
+    <Footer/>
+  </>);
+}
