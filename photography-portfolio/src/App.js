72f4e9af7bc00bfa70d82f600e463b9c10da87d3
Peter Buckingham <peterbuckingham177@gmail.com>
2024-11-08 21:35:19 +0200

    lunch break

--- photography-portfolio/src/App.js
+++ photography-portfolio/src/App.js
@@ -1,10 +1,13 @@
 import Nav from './Components/Nav'
+import Hero from './Components/Hero'
 import './index.css'
 
 function App() {
   return (
     <div>
       <Nav />
+      <Hero />
+      
     </div>
   )
 }
---
+++ photography-portfolio/src/Components/Hero.js
@@ -0,0 +1,10 @@
+
+function Hero() {
+  return (
+    <div className="hero-container">
+      
+    </div>
+  )
+}
+
+export default Hero
