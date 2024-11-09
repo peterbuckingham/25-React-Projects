a18ea52d8daa74b5c818c22c169fdfd1c2df7dbb
Peter Buckingham <peterbuckingham177@gmail.com>
2024-11-09 13:31:50 +0200

    Intro section and intro section buttons are done copy must be inserted and styled

--- photography-portfolio/src/App.js
+++ photography-portfolio/src/App.js
@@ -1,12 +1,14 @@
 import Nav from './Components/Nav'
 import Hero from './Components/Hero'
 import './index.css'
+import Intro from './Components/Intro'
 
 function App() {
   return (
     <div>
       <Nav />
       <Hero />
+      <Intro />
       
     </div>
   )
---
+++ photography-portfolio/src/Components/Intro.js
@@ -0,0 +1,16 @@
+
+
+function Intro() {
+  return (
+    <div className="intro-section">
+      <div className="intro-section1"><p>test</p></div>
+      <div className="intro-section2"><p>test1</p></div>
+      <div className="intro-buttons">
+      <button className="intro-button">Contact</button>
+      <button className="intro-button">About</button>
+      </div>
+    </div>
+  )
+}
+
+export default Intro
