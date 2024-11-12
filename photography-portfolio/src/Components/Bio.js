5c72f412f797dc2ca5c481cd51ed2111a159ae07
Peter Buckingham <peterbuckingham177@gmail.com>
2024-11-12 21:05:14 +0200

    Bio Video Inserted. Next is the image gallery preview

--- photography-portfolio/src/Components/Bio.js
+++ photography-portfolio/src/Components/Bio.js
@@ -5,7 +5,7 @@ function Bio() {
     <div className='bioContainer'>
       <div className="bioSection"><p>Creative Director , Fashion and Model Photographer for Kavyar which hosts all the Top Fashion Magazines Worldwide. Mark’s been active since 1988 and is involved in casting with SA’s  Top Agencies and getting talent published on a Global scale.</p></div>
       <div className='bioDivider'></div>
-      video
+      <video className='bioVideo' autoPlay loop muted src={profilePic}></video>
     </div>
   )
 }
